f88f49a2a01581e88f2ffb816d448b9f8312ef07
azayatsfv <artsiom.zayats@fiserv.com>
2020-08-20 16:41:50 -0700

    Fixed assets/automobile endpoints

--- APIs/client.py
+++ APIs/client.py
@@ -1,9 +1,10 @@
 from base.clients.base_client import BaseClient
 
 from APIs.loans.client import LoanClient
-
+from APIs.assets.client import AssetsClient
 
 class Client(BaseClient):
     def __init__(self, base_url, database, port=None, headers=None):
         super().__init__(base_url=base_url, database=database, port=port, headers=headers)
         self.loan = LoanClient(client=self)
+        self.assets = AssetsClient(client = self)
